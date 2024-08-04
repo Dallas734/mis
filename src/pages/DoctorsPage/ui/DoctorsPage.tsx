df639f9abd91a9e3f444d6c05a07b2b74fd79394
Mark <vernidub66@gmail.com>
2024-08-04 11:04:50 +0300

    Updated doctor and patient pages

--- src/pages/DoctorsPage/ui/DoctorsPage.tsx
+++ src/pages/DoctorsPage/ui/DoctorsPage.tsx
@@ -3,7 +3,7 @@ import { DoctorApi } from "../../../entities/Doctor/api/DoctorApi";
 import { NTable } from "../../../shared/ui/Table";
 import { TableColumn } from "../../../shared/types/TableColumn";
 import { Doctor } from "../../../entities/Doctor";
-import cls from './DoctorsPage.module.scss'
+import cls from "./DoctorsPage.module.scss";
 import { Button } from "../../../shared/ui/Button";
 import classNames from "classnames";
 import { QueryT } from "../../../shared/types/QueryT";
@@ -12,21 +12,21 @@ import { DoctorModal } from "../../../widgets/Modals/DoctorModal";
 export const DoctorsPage = () => {
   const { data: doctors } = DoctorApi.useFetchAllDoctorsQuery();
   const [selectedDoctor, setSelectedDoctor] = useState<Doctor>();
-  const [ deleteDoctor ] = DoctorApi.useDeleteDoctorMutation();
+  const [deleteDoctor] = DoctorApi.useDeleteDoctorMutation();
   const [queryType, setQueryType] = useState<QueryT | undefined>();
   const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
 
   const head: TableColumn[] = [
-    {index: 'lastName', name: 'Фамилия', sortMethod: "asc"},
-    {index: 'firstName', name: 'Имя', sortMethod: "asc"},
-    {index: 'surname', name: 'Отчество', sortMethod: "asc"},
-    {index: 'dateOfBirth', name: 'Дата рождения', sortMethod: "asc"},
-    {index: 'specialization.name', name: 'Специализация', sortMethod: "asc"},
-    {index: 'status.name', name: 'Статус', sortMethod: "asc"},
-    {index: 'area.id', name: 'Участок', sortMethod: "asc"},
-    {index: 'category.name', name: 'Категория', sortMethod: "asc"},
-    {index: 'gender.name', name: 'Пол', sortMethod: "asc"}
-  ]
+    { index: "lastName", name: "Фамилия", sortMethod: "asc" },
+    { index: "firstName", name: "Имя", sortMethod: "asc" },
+    { index: "surname", name: "Отчество", sortMethod: "asc" },
+    { index: "dateOfBirth", name: "Дата рождения", sortMethod: "asc" },
+    { index: "specialization.name", name: "Специализация", sortMethod: "asc" },
+    { index: "status.name", name: "Статус", sortMethod: "asc" },
+    { index: "area.id", name: "Участок", sortMethod: "asc" },
+    { index: "category.name", name: "Категория", sortMethod: "asc" },
+    { index: "gender.name", name: "Пол", sortMethod: "asc" },
+  ];
 
   const createButtonClasses = classNames(
     "icon",
@@ -54,42 +54,57 @@ export const DoctorsPage = () => {
     setSelectedDoctor(undefined);
   };
 
-//   const handleClearFilterButton = () => {
-//     setId("");
-//     setSelectedTypeId("");
-//     setSelectedOwnerId("");
-//     setSelectedBirthday(1995);
-//   };
+  //   const handleClearFilterButton = () => {
+  //     setId("");
+  //     setSelectedTypeId("");
+  //     setSelectedOwnerId("");
+  //     setSelectedBirthday(1995);
+  //   };
 
   const handleCreateButton = () => {
-    setQueryType('CREATE');
+    setQueryType("CREATE");
     setModalIsOpen(true);
-  }
+  };
 
   const handleUpdateButton = () => {
-    setQueryType('UPDATE');
-    setModalIsOpen(true)
-  }
+    setQueryType("UPDATE");
+    setModalIsOpen(true);
+  };
 
   return (
     <>
-    <DoctorModal isOpen={modalIsOpen} setIsOpen={setModalIsOpen} doctor={selectedDoctor} queryType={queryType}/>
-    <div className={cls.fieldsBlock}>
-          <Button children="Создать" classes={createButtonClasses} onClick={handleCreateButton}/>
-          <Button
-            children="Изменить"
-            classes={editButtonClasses}
-            disabled={selectedDoctor ? false : true}
-            onClick={handleUpdateButton}
-          />
-          <Button
-            children="Удалить"
-            classes={deleteButtonClasses}
-            disabled={selectedDoctor ? false : true}
-            onClick={handleDeleteButton}
-          />
-        </div>
-      <NTable head={head} data={doctors} setSelectedElement={setSelectedDoctor}></NTable>
+      <DoctorModal
+        isOpen={modalIsOpen}
+        setIsOpen={setModalIsOpen}
+        doctor={selectedDoctor}
+        queryType={queryType}
+      />
+      <div className={cls.fieldsBlock}>
+        <Button
+          children="Создать"
+          classes={createButtonClasses}
+          onClick={handleCreateButton}
+        />
+        <Button
+          children="Изменить"
+          classes={editButtonClasses}
+          disabled={selectedDoctor ? false : true}
+          onClick={handleUpdateButton}
+        />
+        <Button
+          children="Удалить"
+          classes={deleteButtonClasses}
+          disabled={selectedDoctor ? false : true}
+          onClick={handleDeleteButton}
+        />
+      </div>
+      <div className={cls.page}>
+        <NTable
+          head={head}
+          data={doctors}
+          setSelectedElement={setSelectedDoctor}
+        ></NTable>
+      </div>
     </>
   );
 };
--- src/pages/PatientsPage/ui/PatientsPage.tsx
+++ src/pages/PatientsPage/ui/PatientsPage.tsx
@@ -1,85 +1,103 @@
 import { Patient } from "../../../entities/Patient";
-import { PatientApi } from "../../../entities/Patient/api/PatientApi"
+import { PatientApi } from "../../../entities/Patient/api/PatientApi";
 import { useState } from "react";
 import { QueryT } from "../../../shared/types/QueryT";
 import classNames from "classnames";
-import cls from './PatientsPage.module.scss';
+import cls from "./PatientsPage.module.scss";
 import { Button } from "../../../shared/ui/Button";
 import { TableColumn } from "../../../shared/types/TableColumn";
 import { NTable } from "../../../shared/ui/Table";
 import { PatientModal } from "../../../widgets/Modals/PatientModal";
 
 export const PatientsPage = () => {
-    const { data: patients } = PatientApi.useFetchAllPatientsQuery();
-    const [selectedPatient, setSelectedPatient] = useState<Patient>();
-    const [ deletePatient ] = PatientApi.useDeletePatientMutation();
-    const [queryType, setQueryType] = useState<QueryT | undefined>();
-    const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
+  const { data: patients } = PatientApi.useFetchAllPatientsQuery();
+  const [selectedPatient, setSelectedPatient] = useState<Patient>();
+  const [deletePatient] = PatientApi.useDeletePatientMutation();
+  const [queryType, setQueryType] = useState<QueryT | undefined>();
+  const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
 
-    const head: TableColumn[] = [
-        {index: 'lastName', name: 'Фамилия', sortMethod: "asc"},
-        {index: 'firstName', name: 'Имя', sortMethod: "asc"},
-        {index: 'surname', name: 'Отчество', sortMethod: "asc"},
-        {index: 'gender.name', name: 'Пол', sortMethod: "asc"},
-        {index: 'dateOfBirth', name: 'Дата рождения', sortMethod: "asc"},
-        {index: 'address', name: 'Адрес', sortMethod: "asc"},
-        {index: 'area.id', name: 'Участок', sortMethod: "asc"},
-        {index: 'polis', name: 'Полис', sortMethod: "asc"},
-        {index: 'workPlace', name: 'Место работы', sortMethod: "asc"}
-      ]
+  const head: TableColumn[] = [
+    { index: "lastName", name: "Фамилия", sortMethod: "asc" },
+    { index: "firstName", name: "Имя", sortMethod: "asc" },
+    { index: "surname", name: "Отчество", sortMethod: "asc" },
+    { index: "gender.name", name: "Пол", sortMethod: "asc" },
+    { index: "dateOfBirth", name: "Дата рождения", sortMethod: "asc" },
+    { index: "address", name: "Адрес", sortMethod: "asc" },
+    { index: "area.id", name: "Участок", sortMethod: "asc" },
+    { index: "polis", name: "Полис", sortMethod: "asc" },
+    { index: "workPlace", name: "Место работы", sortMethod: "asc" },
+  ];
 
-      const createButtonClasses = classNames(
-        "icon",
-        "crud",
-        "border-radius",
-        "createButton"
-      ).split(" ");
-    
-      const editButtonClasses = classNames(
-        "icon",
-        "crud",
-        "border-radius",
-        "editButton"
-      ).split(" ");
-    
-      const deleteButtonClasses = classNames(
-        "icon",
-        "crud",
-        "border-radius",
-        "deleteButton"
-      ).split(" ");
+  const createButtonClasses = classNames(
+    "icon",
+    "crud",
+    "border-radius",
+    "createButton"
+  ).split(" ");
 
-      const handleCreateButton = () => {
-        setQueryType('CREATE');
-        setModalIsOpen(true);
-      }
-    
-      const handleUpdateButton = () => {
-        setQueryType('UPDATE');
-        setModalIsOpen(true)
-      }
+  const editButtonClasses = classNames(
+    "icon",
+    "crud",
+    "border-radius",
+    "editButton"
+  ).split(" ");
 
-      const handleDeleteButton = () => {
-        if (selectedPatient) deletePatient(selectedPatient?.id);
-        setSelectedPatient(undefined);
-      }
+  const deleteButtonClasses = classNames(
+    "icon",
+    "crud",
+    "border-radius",
+    "deleteButton"
+  ).split(" ");
 
-    return (<>
-    <PatientModal isOpen={modalIsOpen} setIsOpen={setModalIsOpen} patient={selectedPatient} queryType={queryType}/>
-    <div className={cls.fieldsBlock}>
-          <Button children="Создать" classes={createButtonClasses} onClick={handleCreateButton}/>
-          <Button
-            children="Изменить"
-            classes={editButtonClasses}
-            disabled={selectedPatient ? false : true}
-            onClick={handleUpdateButton}
-          />
-          <Button
-            children="Удалить"
-            classes={deleteButtonClasses}
-            disabled={selectedPatient ? false : true}
-            onClick={handleDeleteButton}
-          />
-        </div>
-      <NTable head={head} data={patients} setSelectedElement={setSelectedPatient}></NTable></>)
-}
+  const handleCreateButton = () => {
+    setQueryType("CREATE");
+    setModalIsOpen(true);
+  };
+
+  const handleUpdateButton = () => {
+    setQueryType("UPDATE");
+    setModalIsOpen(true);
+  };
+
+  const handleDeleteButton = () => {
+    if (selectedPatient) deletePatient(selectedPatient?.id);
+    setSelectedPatient(undefined);
+  };
+
+  return (
+    <>
+      <PatientModal
+        isOpen={modalIsOpen}
+        setIsOpen={setModalIsOpen}
+        patient={selectedPatient}
+        queryType={queryType}
+      />
+      <div className={cls.fieldsBlock}>
+        <Button
+          children="Создать"
+          classes={createButtonClasses}
+          onClick={handleCreateButton}
+        />
+        <Button
+          children="Изменить"
+          classes={editButtonClasses}
+          disabled={selectedPatient ? false : true}
+          onClick={handleUpdateButton}
+        />
+        <Button
+          children="Удалить"
+          classes={deleteButtonClasses}
+          disabled={selectedPatient ? false : true}
+          onClick={handleDeleteButton}
+        />
+      </div>
+      <div className={cls.page}>
+        <NTable
+          head={head}
+          data={patients}
+          setSelectedElement={setSelectedPatient}
+        ></NTable>
+      </div>
+    </>
+  );
+};
