ab09f04c77e5b0817e71c9e1c53ffdc7ad419b2e
Mark <vernidub66@gmail.com>
2024-08-06 17:10:10 +0300

    NotFoundPage modified

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,11 +1,12 @@
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 export const NotFoundPage = () => {
-    return (
-        <section className='section'>
-        <p>такая страница не найдена!</p>
-        <Link to={'/main'}>Главная страница</Link>
+  return (
+    <section className="section">
+      <p>такая страница не найдена!</p>
+      <Link to={"/main"}>Главная страница</Link>
+      <br />
+      <Link to={"/"}>Страница входа</Link>
     </section>
-    );
+  );
 };
